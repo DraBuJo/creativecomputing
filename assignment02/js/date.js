a8eafb876b86f6c35c6760c9148e2557c0742ccc
geng feng <fengg214@newschool.edu>
2018-11-13 20:45:35 -0500

    jb

---
+++ assignment02/js/date.js
@@ -0,0 +1,7 @@
+var answer=(prompt("Why aren’t koalas actual bears?"));
+
+if(answer == "koalafications") {
+	alert("Hey, how did you know?");
+}else {
+	alert("They don’t meet the koalafications.");
+}
